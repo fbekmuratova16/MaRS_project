d6df500dcc19695d919795b2919d440904c2a73b
Alina <alina.kutlushina@pharminnotech.com>
2017-01-19 16:35:52 +0300

    # создать класс, который будет создавать индексы для молекул/реакций. Если реиндекс = False, значит индекс есть, дерево не пересоздаем # если реиндекс = True, значит этого индекса нет, дерево нужно обновить.

--- MARS/TreeIndex.py
+++ MARS/TreeIndex.py
@@ -5,38 +5,35 @@ from pony.orm import select
 from MARS.models import *
 from os import path
 
-fp = select( f.fingerprint for f in Molecules and Reactions)
-id_s = select( i.id for i in Molecules and Reactions)
 dump_dir = 'C://'
 
 class TreeIndex(object):
 
-    def npmatrix(fp):
-        a = []
-        for i in fp:
-            npX = np.fromstring(i, dtype=np.uint8)
-            by_x = np.unpackbits(npX)
-            a.append(by_x)
-        b = np.matrix(a)
-        return b
-
     def __init__(self, data, reindex=False):
         data_path = path.join(dump_dir, '%s.bin' % data.__name__)
-        if reindex or path.exists(data_path):
+
+        if reindex or not path.exists(data_path):
+            a = []
+            ids = []
+            for fp, i in select((s.fingerprint, s.id) for s in data):
+                by_x = np.unpackbits(np.fromstring(fp, dtype=np.uint8))
+                a.append(by_x)
+                ids.append(i)
+
+            tree = BallTree(np.matrix(a), metric='jaccard')
             with open(data_path, 'wb') as f:
-                tree = BallTree(b, metric='jaccard')
-                pickle.dump(tree, f)
+                pickle.dump((tree,ids), f)
         else:
             with open(data_path, 'rb') as f:
-                tree = pickle.load(f)
+                tree, ids = pickle.load(f)
 
         self.__tree = tree
         self.__data = data
+        self.__ids = ids
 
     def get_similar(self, structure):
         q1 = self.__data.get_fingerprints([structure])
-        npq = np.fromstring(q1[0], dtype=np.uint8)
-        by_q = np.unpackbits(npq)
-        q = np.matrix(by_q)
+        by_q = np.unpackbits(np.fromstring(q1[0], dtype=np.uint8))
+        q = np.matrix([by_q])
         dist, ind = self.__tree.query(q, k=10)
-        return dist, ind
+        return dist, [self.__data[self.__ids[x]] for x in ind]
